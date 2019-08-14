4da67d1dc662099e0e41f6743a19de05824141e5
Mamian93 <mamian@autograf.pl>
2019-08-14 18:47:39 +0200

    Half of first module

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,49 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Dominno",
+                    Cuisine = CuisineType.Italian,
+                    Location = "Cracow"
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "Bombi",
+                    Cuisine = CuisineType.Indian,
+                    Location = "Cracow"
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "Turek",
+                    Cuisine = CuisineType.None,
+                    Location = "Wieliczka"
+                },
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(a => a.Name);
+        }
+    }
+}
