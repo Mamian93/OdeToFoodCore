35f8d4343768cf59087f1aac5c52684c13ec4ba0
Mamian93 <mamian@autograf.pl>
2019-08-21 21:10:48 +0200

    Add migrations, partial/viewComponent views

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,98 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Dominno",
+                    Cuisine = CuisineType.Italian,
+                    Location = "Cracow"
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "Bombi",
+                    Cuisine = CuisineType.Indian,
+                    Location = "Cracow"
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "Turek",
+                    Cuisine = CuisineType.None,
+                    Location = "Wieliczka"
+                },
+            };
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            if (restaurant != null)
+            {
+                restaurants.Add(restaurant);
+                restaurant.Id = restaurants.Max(a => a.Id) + 1;
+            }
+
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var res = restaurants.SingleOrDefault(a => a.Id == id);
+            if (res != null)
+            {
+                restaurants.Remove(res);
+            }
+            return res;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(a => a.Name);
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.FirstOrDefault(a => a.Id == id);
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return restaurants.Where(a => string.IsNullOrEmpty(name) || a.Name.StartsWith(name))
+                              .OrderBy(a => a.Name);
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            var res = restaurants.SingleOrDefault(a => a.Id == restaurant.Id);
+            if (res != null)
+            {
+                res.Name = restaurant.Name;
+                res.Location = restaurant.Location;
+                res.Cuisine = restaurant.Cuisine;
+            }
+            return res;
+        }
+    }
+}
---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if (restaurant != null)
+            {
+                _context.Restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return _context.Restaurants.Find(id);
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            return _context.Restaurants.Count();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var query = _context.Restaurants.Where(a => a.Name.StartsWith(name) || string.IsNullOrEmpty(name))
+                                            .OrderBy(a => a.Name);
+            return query;
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            var entity = _context.Restaurants.Attach(restaurant);
+            entity.State = EntityState.Modified;
+            return restaurant;
+        }
+    }
+}
